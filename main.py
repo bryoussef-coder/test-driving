0a355205f052ec5c75d09790e5e679ecf0bdc18d
youssef ben rejeb <youssef.benrejeb@gmail.com>
2022-08-31 23:22:38 +0100

    Ajout de l'execution automatique des test from pytest import main

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from pytest import main
+
+
 def join_01(xs, delimiter: str) -> str:
     re = delimiter.join(str(x) for x in xs)
     return re
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
+#
+#     print(join_01([1, 2, 3], "==="))
 
-    print(join_01([1, 2, 3], "==="))
-
+main(['-vv'])  # c'est pour executer directement les tests
